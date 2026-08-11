7de0042e990e49011f1cca20db926e6d6cde4cd6
User <user@example.com>
2026-08-11 14:37:20 -0300

    se termino el punto 1

---
+++ ej1.ts
@@ -0,0 +1,34 @@
+class Producto {
+
+    nombre: string;
+    precio: number;
+    categoria: string;
+    stock: number;
+
+    constructor(nombre: string, precio: number, categoria: string, stock: number) {
+            this.nombre = nombre;
+            this.precio = precio;
+            this.categoria = categoria;
+            this.stock = stock;
+        }
+    describir(): void {
+        console.log(`Nombre: ${this.nombre}, Precio: ${this.precio}, Categoria: ${this.categoria}, Stock: ${this.stock}`);
+    }
+    hayStock(cantidad: number): boolean {
+        return this.stock > 0 && cantidad <= this.stock ? true : false
+    }
+    venderUnidades(cantidad: number): string | void {
+        
+        const hayUnidades = this.hayStock(cantidad);
+        
+        if(hayUnidades){
+            this.stock = this.stock - 1
+        } else {
+            console.log('No hay stock disponible')
+        }
+    }
+    aplicarDescuento(porcentaje: number): number | void{
+        const descuento = this.precio * (porcentaje / 100)
+        return this.precio - descuento
+    }
+};
