581e8447007a0fdddc212b0ca5328174a0a43153
lucas <natanaelzigaran@gmail.com>
2026-08-11 20:24:45 -0300

    feat: add Empleado and EmpleadoFijo classes with salary calculation

---
+++ ej4.ts
@@ -0,0 +1,29 @@
+class Empleado {
+    protected nombre: string;
+    protected antiguedad: number;
+
+    constructor(nombre: string, antiguedad: number) {
+        this.nombre = nombre;
+        this.antiguedad = antiguedad;
+    }
+
+    calcularSueldo(): number {
+        return 0;
+    }
+
+    describir(): string {
+        return `Empleado: ${this.nombre}, Antigüedad: ${this.antiguedad} años`;
+    }
+}
+
+class EmpleadoFijo extends Empleado {
+    sueldoBase: number
+    
+    constructor(nombre: string, antiguedad: number, sueldoBase: number) {
+        super(nombre, antiguedad);
+        this.sueldoBase = sueldoBase;
+    }
+    calcularSueldo(): number {
+        return this.sueldoBase;
+    }
+}
